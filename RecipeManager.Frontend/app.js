990f22431defd3f68486aed57df125255dc4cdf1
Aditya Praveen Menon <adityapmenon29@gmail.com>
2026-06-10 15:57:34 +0530

    Update frontend to fetch recipes from search query

--- RecipeManager.Frontend/app.js
+++ RecipeManager.Frontend/app.js
@@ -1,7 +1,7 @@
 // DOM elements
-const searchBox = document.getElementById("search-input");
+const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
-const recipeContainer = document.getElementById("recipes");
+const recipesContainer = document.getElementById("recipes");
 const resultHeading = document.getElementById("result-heading");
 const errorContainer = document.getElementById("error-container");
 const recipeDetails = document.getElementById("recipe-details");
@@ -9,14 +9,90 @@ const recipeDetailsContent = document.getElementById("recipe-details-content");
 const backBtn = document.getElementById("back-btn");
 
 const BASE_URL = "http://localhost:5274/api/";
-const SEARCH_URL = `${BASE_URL}`;
-const LOOKUP_URL = `${BASE_URL}`;
+const SEARCH_URL = `${BASE_URL}recipes?search=`;
+const LOOKUP_URL = `${BASE_URL}recipes/`; // get recipe details from id
 
 searchBtn.addEventListener("click", searchMeals);
 searchInput.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") searchMeals()
+  if (e.key === "Enter") searchMeals();
 });
 
-function searchMeals() {
-    
+async function searchMeals() {
+  const searchTerm = searchInput.value.trim();
+
+  if (!searchTerm) {
+    errorContainer.textContent = "Please enter a search term";
+    errorContainer.classList.remove("hidden");
+    return;
+  }
+
+  try {
+    resultHeading.textContent = `Searching "${searchTerm}"...`;
+    recipesContainer.innerHTML = "";
+    errorContainer.classList.add("hidden");
+
+    const response = await fetch(`${SEARCH_URL}${searchTerm}`);
+    const recipesResponseArray = await response.json();
+
+    if (recipesResponseArray.length == 0) {
+      resultHeading.textContent = "";
+      recipesContainer.innerHTML = "";
+      errorContainer.textContent = `No recipes found for "${searchTerm}".
+                Try another search term!`;
+      errorContainer.classList.remove("hidden");
+    } else {
+      resultHeading.textContent = `Search results for "${searchTerm}":`;
+      await displayRecipes(recipesResponseArray);
+      searchInput.value = "";
+    }
+  } catch (error) {
+    errorContainer.textContent =
+      "Something went wrong. Please try again later.";
+    errorContainer.classList.remove("hidden");
+  }
+}
+
+async function displayRecipes(recipesResponseArray) {
+  recipesContainer.innerHTML = "";
+  const recipesArray = await fetchRecipes(recipesResponseArray);
+
+  recipesArray.forEach((recipe) => {
+    let recipeTags = "";
+    recipe["tags"].forEach((tag) => {
+      recipeTags += `<span class="tag">${tag}</span>`;
+    });
+
+    let recipeCategories = "";
+    recipe["categories"].forEach((category) => {
+      recipeCategories += `<span class="category">${category}</span>`
+    });
+
+    recipesContainer.innerHTML += `
+      <div class="recipe" data-recipe-id="${recipe.recipeId}">
+        <div class="recipe-info">
+          <h3 class="recipe-title">${recipe.recipeName}</h3>
+          <div class="recipe-categories">
+            ${recipeCategories}
+          </div>
+          <div class="recipe-tags">
+            ${recipeTags}
+          </div>
+        </div>
+      </div>
+    `;
+  });
+}
+
+async function fetchRecipes(recipesResponseArray) {
+  recipesArray = [];
+  for (var i = 0; i < recipesResponseArray.length; i++) {
+    recipesArray.push(await fetchRecipeById(recipesResponseArray[i].recipeId));
+  }
+  return recipesArray;
+}
+
+async function fetchRecipeById(recipeId) {
+  var response = await fetch(`${LOOKUP_URL}${recipeId}`);
+  var recipe = response.json();
+  return recipe;
 }
